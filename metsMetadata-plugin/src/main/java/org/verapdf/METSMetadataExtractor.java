e8bca0e16f4b2f01ad14d77f482bc5d538101658
Maksim Bezrukov <bezrukovm95@gmail.com>
2016-03-10 16:06:29 +0300

    mets plugin uncomplited

---
+++ metsMetadata-plugin/src/main/java/org/verapdf/METSMetadataExtractor.java
@@ -0,0 +1,224 @@
+package org.verapdf;
+
+import au.edu.apsr.mtk.base.*;
+import com.adobe.xmp.XMPException;
+import com.adobe.xmp.XMPMeta;
+import com.adobe.xmp.XMPMetaFactory;
+import com.adobe.xmp.impl.VeraPDFMeta;
+import com.adobe.xmp.impl.VeraPDFXMPNode;
+import com.adobe.xmp.impl.XMPSchemaRegistryImpl;
+import com.sun.xml.internal.messaging.saaj.util.ByteInputStream;
+import org.verapdf.core.FeatureParsingException;
+import org.verapdf.features.AbstractMetadataFeaturesExtractor;
+import org.verapdf.features.MetadataFeaturesData;
+import org.verapdf.features.tools.FeatureTreeNode;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.*;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Maksim Bezrukov
+ */
+public class METSMetadataExtractor extends AbstractMetadataFeaturesExtractor {
+
+    private XMPMeta dmdMeta = null;
+    private XMPMeta rightsMeta = null;
+    private XMPMeta techMeta = null;
+    private XMPMeta sourceMeta = null;
+    private XMPMeta digiprovMeta = null;
+
+    @Override
+    public List<FeatureTreeNode> getMetadataFeatures(MetadataFeaturesData metadataFeaturesData) {
+        List<FeatureTreeNode> result = new ArrayList<>(1);
+        try {
+            File outFile = getOutFile();
+            byte[] bytes = metadataFeaturesData.getStream();
+            convertXMPToMETS(new ByteInputStream(bytes, bytes.length), new FileOutputStream(outFile));
+            FeatureTreeNode node = FeatureTreeNode.createRootNode("resultFile");
+            node.setValue(outFile.getCanonicalPath());
+            result.add(node);
+        } catch (XMPException | METSException | IOException | SAXException | FeatureParsingException | ParserConfigurationException e) {
+            try {
+                FeatureTreeNode node = FeatureTreeNode.createRootNode("Error");
+                node.setValue(e.getMessage());
+                result.add(node);
+            } catch (FeatureParsingException e1) {
+                throw new IllegalStateException(e1);
+            }
+        }
+        return result;
+    }
+
+    private void convertXMPToMETS(InputStream toConvert, OutputStream out) throws XMPException, METSException, IOException, SAXException, ParserConfigurationException {
+        VeraPDFMeta meta = VeraPDFMeta.parse(toConvert);
+        divideMetas(meta);
+        METSWrapper metsWrapper = new METSWrapper();
+        createMETS(metsWrapper);
+        metsWrapper.validate();
+        metsWrapper.write(out);
+    }
+
+    private void createMETS(METSWrapper metsWrapper) throws METSException, SAXException, ParserConfigurationException, XMPException, IOException {
+        METS mets = metsWrapper.getMETSObject();
+
+        DmdSec dmd = mets.newDmdSec();
+        MdWrap dmdWrap = dmd.newMdWrap();
+        addXMPTreeToMdWrap(dmdWrap, this.dmdMeta);
+        dmd.setMdWrap(dmdWrap);
+        mets.addDmdSec(dmd);
+
+        AmdSec amd = mets.newAmdSec();
+
+        RightsMD rightsMD = amd.newRightsMD();
+        MdWrap rightsWrap = rightsMD.newMdWrap();
+        addXMPTreeToMdWrap(rightsWrap, this.rightsMeta);
+        rightsMD.setMdWrap(rightsWrap);
+        amd.addRightsMD(rightsMD);
+
+        TechMD techMD = amd.newTechMD();
+        MdWrap techWrap = techMD.newMdWrap();
+        addXMPTreeToMdWrap(techWrap, this.techMeta);
+        techMD.setMdWrap(techWrap);
+        amd.addTechMD(techMD);
+
+        SourceMD sourceMD = amd.newSourceMD();
+        MdWrap sourceWrap = sourceMD.newMdWrap();
+        addXMPTreeToMdWrap(sourceWrap, this.sourceMeta);
+        sourceMD.setMdWrap(sourceWrap);
+        amd.addSourceMD(sourceMD);
+
+        DigiprovMD digiprovMD = amd.newDigiprovMD();
+        MdWrap digiprovWrap = digiprovMD.newMdWrap();
+        addXMPTreeToMdWrap(digiprovWrap, this.rightsMeta);
+        digiprovMD.setMdWrap(digiprovWrap);
+        amd.addDigiprovMD(digiprovMD);
+
+        mets.addAmdSec(amd);
+    }
+
+    private void addXMPTreeToMdWrap(MdWrap wrap, XMPMeta meta) throws XMPException, ParserConfigurationException, IOException, SAXException {
+        ByteArrayOutputStream bstream = new ByteArrayOutputStream();
+        XMPMetaFactory.serialize(meta, bstream);
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse(new ByteArrayInputStream(bstream.toByteArray()));
+        wrap.setXmlData(doc.getDocumentElement());
+    }
+
+    private void divideMetas(VeraPDFMeta meta) {
+
+        List<VeraPDFXMPNode> xmpProps = meta.getProperties();
+        List<String[]> properties;
+
+        if (meta.getExtensionSchemasNode() != null) {
+            properties = new ArrayList<>(xmpProps.size() + 1);
+            properties.add(new String[]{XMPSchemaRegistryImpl.NS_PDFA_EXTENSION, "schemas"});
+        } else {
+            properties = new ArrayList<>(xmpProps.size());
+        }
+
+        for (VeraPDFXMPNode node : xmpProps) {
+            properties.add(new String[]{node.getNamespaceURI(), node.getName()});
+        }
+
+        this.dmdMeta = meta.getCloneOfInitialMeta();
+        this.rightsMeta = meta.getCloneOfInitialMeta();
+        this.techMeta = meta.getCloneOfInitialMeta();
+        this.sourceMeta = meta.getCloneOfInitialMeta();
+        this.digiprovMeta = meta.getCloneOfInitialMeta();
+
+        boolean isDMDEmpty = true;
+        boolean isRightsEmpty = true;
+        boolean isTechEmpty = true;
+        boolean isSourceEmpty = true;
+        boolean isDIGIPROVEmpty = true;
+
+        for (String[] el : properties) {
+            switch (METSTypeRegistry.getTypeForProperty(el[0], el[1])) {
+                case DMD_SEC:
+                    isDMDEmpty = false;
+                    rightsMeta.deleteProperty(el[0], el[1]);
+                    techMeta.deleteProperty(el[0], el[1]);
+                    sourceMeta.deleteProperty(el[0], el[1]);
+                    digiprovMeta.deleteProperty(el[0], el[1]);
+                    break;
+                case RIGHTS_MD:
+                    isRightsEmpty = false;
+                    dmdMeta.deleteProperty(el[0], el[1]);
+                    techMeta.deleteProperty(el[0], el[1]);
+                    sourceMeta.deleteProperty(el[0], el[1]);
+                    digiprovMeta.deleteProperty(el[0], el[1]);
+                    break;
+                case TECH_MD:
+                    isTechEmpty = false;
+                    dmdMeta.deleteProperty(el[0], el[1]);
+                    rightsMeta.deleteProperty(el[0], el[1]);
+                    sourceMeta.deleteProperty(el[0], el[1]);
+                    digiprovMeta.deleteProperty(el[0], el[1]);
+                    break;
+                case SOURCE_MD:
+                    isSourceEmpty = false;
+                    dmdMeta.deleteProperty(el[0], el[1]);
+                    rightsMeta.deleteProperty(el[0], el[1]);
+                    techMeta.deleteProperty(el[0], el[1]);
+                    digiprovMeta.deleteProperty(el[0], el[1]);
+                    break;
+                case DIGIPROV_MD:
+                    isDIGIPROVEmpty = false;
+                    dmdMeta.deleteProperty(el[0], el[1]);
+                    rightsMeta.deleteProperty(el[0], el[1]);
+                    techMeta.deleteProperty(el[0], el[1]);
+                    sourceMeta.deleteProperty(el[0], el[1]);
+                    break;
+            }
+        }
+
+        if (isDMDEmpty) {
+            this.dmdMeta = null;
+        }
+        if (isRightsEmpty) {
+            this.rightsMeta = null;
+        }
+        if (isTechEmpty) {
+            this.techMeta = null;
+        }
+        if (isSourceEmpty) {
+            this.sourceMeta = null;
+        }
+        if (isDIGIPROVEmpty) {
+            this.digiprovMeta = null;
+        }
+    }
+
+    private File getOutFile() {
+        File fold = getFolderPath().toFile();
+        File outFold = new File(fold, "out");
+        if (!outFold.exists() && !outFold.mkdir()) {
+            throw new IllegalStateException("Can not create output directory");
+        }
+
+        File res = new File(outFold, "mets.xml");
+        int i = 0;
+        while (res.exists()) {
+            res = new File(outFold, "mets_" + ++i + ".xml");
+        }
+        return res;
+    }
+
+    @Override
+    public String getID() {
+        return "63f0a295-587b-4e50-909c-4b47e02f64f7";
+    }
+
+    @Override
+    public String getDescription() {
+        return "This extractor generates METS file based on XMP package.";
+    }
+}
---
+++ metsMetadata-plugin/src/main/java/org/verapdf/METSTypeRegistry.java
@@ -0,0 +1,139 @@
+package org.verapdf;
+
+import com.adobe.xmp.impl.XMPSchemaRegistryImpl;
+
+/**
+ * @author Maksim Bezrukov
+ */
+public class METSTypeRegistry {
+
+    public enum METSType {
+        DMD_SEC,
+        RIGHTS_MD,
+        TECH_MD,
+        SOURCE_MD,
+        DIGIPROV_MD
+    }
+
+    public static METSType getTypeForProperty(String namespace, String name) {
+
+        switch (namespace) {
+            case XMPSchemaRegistryImpl.NS_DC:
+                return getDCType(name);
+            case XMPSchemaRegistryImpl.NS_XMP:
+                return getXMPType(name);
+            case XMPSchemaRegistryImpl.TYPE_IDENTIFIERQUAL:
+                return METSType.TECH_MD;
+            case XMPSchemaRegistryImpl.NS_XMP_RIGHTS:
+                return METSType.RIGHTS_MD;
+            case XMPSchemaRegistryImpl.NS_XMP_MM:
+                return METSType.DIGIPROV_MD;
+            case XMPSchemaRegistryImpl.NS_XMP_BJ:
+            case XMPSchemaRegistryImpl.TYPE_PAGEDFILE:
+            case XMPSchemaRegistryImpl.NS_PHOTOSHOP:
+            case XMPSchemaRegistryImpl.NS_CAMERARAW:
+            case XMPSchemaRegistryImpl.NS_EXIF:
+            case XMPSchemaRegistryImpl.NS_EXIF_AUX:
+                return METSType.TECH_MD;
+            case XMPSchemaRegistryImpl.NS_DM:
+                return getDMType(name);
+            case XMPSchemaRegistryImpl.NS_PDF:
+                return getPDFType(name);
+            case XMPSchemaRegistryImpl.NS_TIFF:
+                return getTIFFType(name);
+            default:
+                return METSType.TECH_MD;
+        }
+    }
+
+    private static METSType getDCType(String name) {
+        switch (name) {
+            case "description":
+            case "language":
+            case "relation":
+            case "source":
+            case "subject":
+            case "title":
+            case "type":
+                return METSType.DMD_SEC;
+            case "contributor":
+            case "creator":
+            case "publisher":
+            case "rights":
+                return METSType.RIGHTS_MD;
+            case "coverage":
+            case "format":
+            case "identifier":
+                return METSType.TECH_MD;
+            case "date":
+                return METSType.DIGIPROV_MD;
+            default:
+                return METSType.TECH_MD;
+        }
+    }
+
+    private static METSType getXMPType(String name) {
+        switch (name) {
+            case "Nickname":
+                return METSType.DMD_SEC;
+            case "BaseURL":
+            case "Identifier":
+            case "Label":
+            case "Rating":
+            case "Thumbnails":
+                return METSType.TECH_MD;
+            case "CreatorTool":
+                return METSType.SOURCE_MD;
+            case "Advisory":
+            case "CreateDate":
+            case "MetadataDate":
+            case "ModifyDate":
+                return METSType.DIGIPROV_MD;
+            default:
+                return METSType.TECH_MD;
+        }
+    }
+
+    private static METSType getDMType(String name) {
+        switch (name) {
+            case "copyright":
+                return METSType.RIGHTS_MD;
+            case "projectRef":
+                return METSType.SOURCE_MD;
+            case "videoModDate":
+            case "audioModDate":
+            case "metadataModDate":
+                return METSType.DIGIPROV_MD;
+            default:
+                return METSType.TECH_MD;
+        }
+    }
+
+    private static METSType getPDFType(String name) {
+        switch (name) {
+            case "Keywords":
+            case "PDFVersion":
+                return METSType.TECH_MD;
+            case "Producer":
+                return METSType.SOURCE_MD;
+            default:
+                return METSType.TECH_MD;
+        }
+    }
+
+    private static METSType getTIFFType(String name) {
+        switch (name) {
+            case "ImageDescription":
+                return METSType.DMD_SEC;
+            case "Artist":
+            case "Copyright":
+                return METSType.RIGHTS_MD;
+            case "Software":
+                return METSType.SOURCE_MD;
+            case "DateTime":
+                return METSType.DIGIPROV_MD;
+            default:
+                return METSType.TECH_MD;
+        }
+    }
+}
